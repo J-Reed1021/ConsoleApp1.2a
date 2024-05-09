8d94a4d937206d7de2497836b66398dbf67c1b1e
joshu <joshu@REEDPC>
2024-05-08 20:13:19 -0500

    Add project files.

---
+++ ConsoleApp1.2a/Program.cs
@@ -0,0 +1,30 @@
+﻿// Program to accept two integers
+
+namespace ConsoleApp1._2a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int sum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int sum2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sum1 == sum2)
+            {
+                Console.WriteLine("The two numbers you entered are equal");
+            }
+
+
+            else
+            {
+                Console.WriteLine("The two numbers you entered are not equal");
+
+            }
+
+        }
+
+    }
+
+}
